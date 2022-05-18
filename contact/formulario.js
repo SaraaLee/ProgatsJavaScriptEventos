589fc9620699fcbb0b57e1d1bef54f387f1b4f7f
Sara Rodriguez <s.rodriguez91s@hotmail.com>
2022-05-18 09:42:13 +0200

    Progats funciones-bucles acabados

--- contact/formulario.js
+++ contact/formulario.js
@@ -8,9 +8,17 @@ function enviarFormulario(e) {
     // Porgats quiere recibir un correo con el nombre y el asunto 
 
     let nombre = document.querySelector("#fname").value;
+    let apellido = document.querySelector("#lname").value;
     let asunto = document.querySelector("#subject").value;
+    let pais = document.querySelector("#country").value;
 
     // Simular que enviamos un correo usando el console.log
-    console.log(`Hola soy ${nombre} y estoy preocupado porque ${asunto}`);
+    console.log(`Hola soy ${nombre, apellido}, te escribo desde ${pais} y estoy preocupado porque ${asunto}`);
 
+}
+
+function formEnviado() {
+    document.querySelector("#mensaje-gracias").style.display = "block"
+    document.querySelector("form").style.display = "none"
+    document.querySelector("h3").style.display = "none"
 }
